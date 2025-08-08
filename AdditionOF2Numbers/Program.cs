6fe28ffb127281a85b286527524875dd376991f3
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-08 22:36:55 +0530

    Program

---
+++ AdditionOF2Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOF2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum = 0;
+
+            Console.WriteLine("Enter Value 1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value 2:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+
+            Console.WriteLine("Sum Of Given Number " + a + " And " + b + " is:" + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
