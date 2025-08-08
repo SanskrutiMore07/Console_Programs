13a0f6132e0a362ff2318b382d50dfbf581edc9c
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-08-08 22:38:04 +0530

    Program

---
+++ Count_Of_Douplicate_Value/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Of_Douplicate_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3,1,2,3,4 };
+            int i =0, j = 0;
+            Console.WriteLine("Count Of Double Value:");
+            for(i=0;i<numbers.Length;i++)
+            {
+                int cnt = 0;
+                for(j=0;j<numbers.Length;j++)
+                {
+                    if(numbers[i] == numbers[j])
+                    cnt++;
+                }
+                if(cnt>1)
+                {
+                    Console.WriteLine(numbers[i] + " ocours " + cnt + " Times ");
+                   
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Count_Of_Douplicate_Value/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Count_Of_Douplicate_Value")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Count_Of_Douplicate_Value")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d697aa7e-7ae3-4932-b59c-3d69b9d39e7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
